60b6481c418e00edd8cd2c04bcab358c0f705595
Binyan Li <binyanli@microsoft.com>
2022-11-09 10:48:37 -0800

    Play with screenshot.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hi");
+const puppeteer = require('puppeteer');
+
+async function run(){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://leetcode.com/problems/total-cost-to-hire-k-workers/');
+
+    await page.screenshot({path: 'example.png', fullPage: true});
+
+    await browser.close();
+}
+
+run();
